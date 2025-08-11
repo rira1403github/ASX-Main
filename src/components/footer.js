d49568b90a71d48911169eb249b25e9a4992cfa9
Ritik Raj <rajritik1403@gmail.com>
2025-08-11 15:59:42 +0530

    second test

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './footer.css';
-import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube, FaTwitch } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
 import {  FaXTwitter } from 'react-icons/fa6';
 import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 // import { SiTiktok } from 'react-icons/si';
@@ -58,14 +58,23 @@ const Footer = () => {
         ))}
       </div>
 
-      <div className="social-footer">
-        <div className="social-item"><FaFacebookF /> <span>FACEBOOK</span></div>
-        <div className="social-item"><FaInstagram /> <span>INSTAGRAM</span></div>
-        <div className="social-item"><FaLinkedinIn /> <span>LINKEDIN</span></div>
-        <div className="social-item"><FaTwitch /> <span>TWITCH</span></div>
-        <div className="social-item"><FaXTwitter /> <span>TWITTER</span></div>
-        <div className="social-item"><FaYoutube /> <span>YOUTUBE</span></div>
-      </div>
+       <div className="social-footer">
+        <a href="https://www.facebook.com/people/A-thon/61567067664336/?rdid=KhKFvb5di8wMmWAe&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2FcND9rTs3aHFeZcEi" target="_blank" rel="noopener noreferrer" className="social-item">
+        <FaFacebookF /> <span>FACEBOOK</span>
+        </a>
+        <a href="https://www.instagram.com/athon_allterrain/" target="_blank" rel="noopener noreferrer" className="social-item">
+        <FaInstagram /> <span>INSTAGRAM</span>
+        </a>
+        <a href="https://www.linkedin.com/company/athonat/posts/?feedView=all" target="_blank" rel="noopener noreferrer" className="social-item">
+        <FaLinkedinIn /> <span>LINKEDIN</span>
+        </a>
+        <a href="https://x.com/ATHON1705" target="_blank" rel="noopener noreferrer" className="social-item">
+        <FaXTwitter /> <span>TWITTER</span>
+        </a>
+        <a href="https://www.youtube.com/@a-thon" target="_blank" rel="noopener noreferrer" className="social-item">
+        <FaYoutube /> <span>YOUTUBE</span>
+        </a>
+        </div>
 
       <div className="footer-bottom">
         <div className="footer-links">
